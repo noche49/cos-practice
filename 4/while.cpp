195d7c270ce6359e75768a97b5f38025b97fa9e6
noche49 <nochan@kentech.ac.kr>
2026-03-26 01:18:51 +0000

    test commit

--- 4/while.cpp
+++ 4/while.cpp
@@ -9,10 +9,11 @@ int main()
 	svar = 20000;
 	ivar = 20000;
 	uvar = 20000;
-
-	for (i=0; i<7; i++) {
-		cout << "svar: " << svar << "\tivar: " << ivar << "\tuvar " << uvar << endl;
+	i=0;
+	while (i<7) {
+		cout << "svar: " << svar << "\tivar: " << ivar << "\tuvar: " << uvar << endl;
 		svar -= 20000; ivar -= 20000; uvar -= 20000;
+		i++;
 	}
 	return 0;
 }
